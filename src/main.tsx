8b073f9c4a1f734b8af2200bebf3eabef5bf6062
Greengrass <gpeengpass@gmail.com>
2025-11-14 00:18:30 +0500

    feat: add tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client'
 
+import './index.css'
+
 createRoot(document.getElementById('root')!).render(
-  <h1>Hello, world!</h1>
+  <h1 className='text-red-600'>Hello, world!</h1>
 )
